82560af41ab9d705a2cca956ad1d63f651601ac0
failedpeanut <failedpeanut@gmail.com>
2021-06-13 11:00:47 +0530

    springboot

--- spring-boot/src/main/java/com/failedpeanut/Application.java
+++ spring-boot/src/main/java/com/failedpeanut/Application.java
@@ -1,4 +1,4 @@
-package com.failedpeanut.configclient;
+package com.failedpeanut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
